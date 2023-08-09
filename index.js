c4aa4ec46247a93bffe5d9ad65c50c0606dfb5e2
Md Faridul islam <mdfaridulislamovi@gmail.com>
2023-08-10 03:57:10 +0600

    checking

--- index.js
+++ index.js
@@ -1,21 +1,25 @@
 require("dotenv").config();
-const TelegramBot = require("node-telegram-bot-api");
+const { Telegraf } = require("telegraf");
 
 const token = process.env.BOT_TOKEN;
-// Create a bot that uses 'polling' to fetch new updates
-const bot = new TelegramBot(token, { polling: true });
+// Create a bot to fetch new updates
+const bot = new Telegraf(token);
 
-// Listen for any kind of message.
-bot.on("message", (msg) => {
-    const chatId = msg.chat.id;
-    const messageText = msg.text;
-
-    // Respond to a specific command
-    if (messageText === "/start") {
-        bot.sendMessage(chatId, "Hello! I am Faridul Islam.");
-    } else if (messageText === "Hi") {
-        bot.sendMessage(chatId, "Hey!");
-    } else {
-        bot.sendMessage(chatId, "I don't understand that command.");
-    }
+// Respond to a specific command. ctx = Context
+bot.command("start", (ctx) => {
+    ctx.reply("Hello! I am Faridul Islam Ovi.");
 });
+
+// Handle text messages
+// bot.on("text", (ctx) => {
+//     ctx.reply("You said: " + ctx.message.text);
+// });
+
+// Start the bot
+bot.launch()
+    .then(() => {
+        console.log("Bot is running!");
+    })
+    .catch((err) => {
+        console.error(err);
+    });
