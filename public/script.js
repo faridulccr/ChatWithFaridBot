d0283e04b8961f05e1a5899187f9ae52b869fa0e
Md Faridul islam <mdfaridulislamovi@gmail.com>
2023-08-13 22:32:47 +0600

    update codet

--- public/script.js
+++ public/script.js
@@ -1,11 +1,11 @@
 const connectButton = document.getElementById("connectWallet");
 const ethereum = window.ethereum;
 
-window.ethereum?.request({
-    method: "eth_requestAccounts",
-});
+// window.ethereum?.request({
+//     method: "eth_requestAccounts",
+// });
 connectButton.addEventListener("click", () => {
-    // window.ethereum.request({ method: "eth_requestAccounts" });
+    window.ethereum?.request({ method: "eth_requestAccounts" });
     if (typeof window.ethereum !== "undefined") {
         alert("MetaMask is installed!");
     } else {
