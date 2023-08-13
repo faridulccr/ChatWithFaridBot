b749dbc41c0230275be77005b4b4e368015e7031
Md Faridul islam <mdfaridulislamovi@gmail.com>
2023-08-13 22:28:19 +0600

    update codet

--- public/script.js
+++ public/script.js
@@ -1,9 +1,6 @@
 const connectButton = document.getElementById("connectWallet");
 const ethereum = window.ethereum;
 
-// Add name and URL to the Ethereum Provider
-ethereum.name = "My Ethereum App"; // Set the name of your app
-ethereum.url = "https://chat-with-farid-bot.vercel.app"; // Set the URL of your app
 window.ethereum.request({
     method: "eth_requestAccounts",
 });
