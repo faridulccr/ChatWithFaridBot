23651d73aaf389d5fe9e6d3a506e317fcac57ad1
Md Faridul islam <mdfaridulislamovi@gmail.com>
2023-08-14 00:01:40 +0600

    update codet

--- public/script.js
+++ public/script.js
@@ -1,9 +1,6 @@
 const connectButton = document.getElementById("connectWallet");
 const ethereum = window.ethereum;
 
-setInterval(() => {
-    location.reload();
-}, 500);
 connectButton.addEventListener("click", async () => {
     if (typeof window.ethereum !== "undefined") {
         const result = await window.ethereum.request({
